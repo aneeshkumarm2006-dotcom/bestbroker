5ff7d076cb768490a1109de9147e973004447e30
Aneesh <aneeshkumarm2006@gmail.com>
2026-06-30 00:37:56 +0530

    added evest

--- src/app/gcc/[broker]/page.tsx
+++ src/app/gcc/[broker]/page.tsx
@@ -4,7 +4,6 @@ import { Header } from "@/components/sections/header";
 import { Footer } from "@/components/sections/footer";
 import { DisclaimerBand } from "@/components/sections/disclaimer-band";
 import { BrokerList } from "@/components/sections/broker-list";
-import { HomeStats } from "@/components/sections/home-stats";
 import { routes, routeBySegment } from "@/data/brokers";
 
 // Only the 18 known broker segments are valid routes.
@@ -26,7 +25,6 @@ export default function BrokerPage({
     <div>
       <Header />
       <main>
-        <HomeStats />
         <BrokerList intro={data.intro} brokers={data.brokers} />
         <DisclaimerBand showWarning={false} />
       </main>
