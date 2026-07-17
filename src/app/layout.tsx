4abca66e867e498a50ebb40c0c3d484cdf63d937
Aneesh <aneeshkumarm2006@gmail.com>
2026-07-18 00:13:23 +0530

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ar" dir="rtl" className={cairo.variable}>
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KH67B6VM');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className="bg-surface font-sans text-ink antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KH67B6VM"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <LanguageProvider>
           {children}
         </LanguageProvider>
